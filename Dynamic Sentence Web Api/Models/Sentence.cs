ffeeb0df3b739fd8869dab1d9d52323b7e1c3dda
oddizzo <oddizzo@gmail.com>
2022-08-13 22:16:55 +0200

    Clean App

--- Dynamic Sentence Web Api/Models/Sentence.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dynamic_Sentence_Web_Api.Models
-{
-    public class Sentence
-    {
-        public int Id { get; set; }
-        public string Words { get; set; }
-    }
-}
--- Dynamic Sentence Web Api/Models/WordType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dynamic_Sentence_Web_Api.Models
-{
-    public class WordType
-    {
-        public int Id { get; set; }
-        public string Type { get; set; }
-    }
-}

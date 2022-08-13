1f8ea8f990a757babf64806ddc0346bb9fa7cfd3
oddizzo <oddizzo@gmail.com>
2022-08-13 22:09:23 +0200

    Add project files.

---
+++ Dynamic Sentence Web Api/Controllers/WordTypesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Dynamic_Sentence_Web_Api.Database;
+using Dynamic_Sentence_Web_Api.Models;
+
+namespace Dynamic_Sentence_Web_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordTypesController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public WordTypesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/WordTypes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<WordType>>> GetWordTypes()
+        {
+            return await _context.WordTypes.ToListAsync();
+        }
+
+        // GET: api/WordTypes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<WordType>> GetWordType(int id)
+        {
+            var wordType = await _context.WordTypes.FindAsync(id);
+
+            if (wordType == null)
+            {
+                return NotFound();
+            }
+
+            return wordType;
+        }
+
+        // PUT: api/WordTypes/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutWordType(int id, WordType wordType)
+        {
+            if (id != wordType.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(wordType).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WordTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/WordTypes
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<WordType>> PostWordType(WordType wordType)
+        {
+            _context.WordTypes.Add(wordType);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetWordType", new { id = wordType.Id }, wordType);
+        }
+
+        // DELETE: api/WordTypes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<WordType>> DeleteWordType(int id)
+        {
+            var wordType = await _context.WordTypes.FindAsync(id);
+            if (wordType == null)
+            {
+                return NotFound();
+            }
+
+            _context.WordTypes.Remove(wordType);
+            await _context.SaveChangesAsync();
+
+            return wordType;
+        }
+
+        private bool WordTypeExists(int id)
+        {
+            return _context.WordTypes.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Dynamic Sentence Web Api/Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
+using Dynamic_Sentence_Web_Api.Database;
+
+namespace Dynamic_Sentence_Web_Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DatabaseConnection")));
+            services.AddCors(c => { c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); });
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+                .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
+            services.AddControllers();
+            services.AddControllers();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
